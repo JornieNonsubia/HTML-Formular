46d9432c08cfd316802ec7bcee4cd4af052bfb88
Mykhailo <misha09107@gmail.com>
2026-05-20 11:25:51 +0200

    changed classes to bootstrap

--- Email-geschikt.php
+++ Email-geschikt.php
@@ -2,13 +2,25 @@
 <body>
 
 <div>
+    <?php
+    $EmailAnrede = filter_var($_POST["anrede"],FILTER_SANITIZE_SPECIAL_CHARS);
+    $EmailSenderName = filter_var($_POST["sender-name"],FILTER_SANITIZE_SPECIAL_CHARS);
+    $EmailSenderMailbox = filter_var($_POST["sender-email"], FILTER_VALIDATE_EMAIL, FILTER_SANITIZE_SPECIAL_CHARS);
+    $EmailSenderNachricht = filter_var($_POST["sender-nachricht"],FILTER_SANITIZE_SPECIAL_CHARS);
 
-<?php echo $_POST["anrede"]; ?> <?php echo $_POST["sender-name"]?>
+
+    ?>
+
+<?php echo $EmailAnrede; ?> <?php echo $EmailSenderName?>
 <br/>
-Ihre E-Mail hat an <?php echo $_POST["sender-email"]; ?> geschikt.
+Ihre E-Mail hat an <?php
+    if(filter_var($EmailSenderMailbox, FILTER_VALIDATE_EMAIL))
+        echo $EmailSenderMailbox;
+else echo "Invalid E-Mail"
+    ?> geschikt.
 <br/>
 Ihre Nachricht lautet:<br/>
-"<?php echo $_POST["sender-nachricht"]; ?>"
+"<?php echo $EmailSenderNachricht; ?>"
 <br/><br/>
     Vielen Dank fuer Nutzung unsere tolle E-Mail Service.
     <br/>
@@ -17,3 +29,4 @@ Ihre Nachricht lautet:<br/>
 
 </body>
 </html>
+
--- index.php
+++ index.php
@@ -24,42 +24,9 @@
             background-color: var(--colors--shapeblau);
             color: var(--colors--shapegrau)
         }
-        form{
-            text-align: center;
-            padding-top: 5%;
-            padding-bottom: 5%;
-        }
-        form p{margin:0}
-        .form-div-block{
-            margin-bottom: 25px;
-            padding: 0 10% 0 10%;
-            text-align: left;
-        }
-        .form-div-block-input{
-
-        }
-        .form-div-block-radio{
-            margin-right: 8px;
-        }
         #sender-nachricht{
             min-height: 200px;
             max-height: 400px;
-            text-align: start;
-        }
-        .email-text-field{
-            min-width: 100%;
-            max-width: 100%;
-            box-shadow: 1px 1px 4px 0 black;
-            border-style: none;
-            border-radius: 10px;
-        }
-        form button {
-            border-radius: 10px;
-            border-width: 1px;
-            border-style: solid;
-            border-color: var(--colors--shapegrau-mittel);
-            color: var(--colors--shapegrau);
-            background-color: var(--colors--shapegrun);
         }
         #div-email-form
         {
@@ -68,35 +35,40 @@
             margin-right: var(--margin--sides);
             min-height: 100vh;
         }
-
     </style>
 </head>
 <body>
 
-<div id="div-email-form">
+<div id="div-email-form" class="pe-4 pt-4 ps-4">
     <form action="Email-geschikt.php" method="post" name="email-schicken-form">
-        <div class="form-div-block f" style="display: flex;">
-            <div>Anrede:</div>
-            <input type="radio" id="herr-andrede" name="anrede" value="Herr">
-            <label for="herr-andrede">Herr</label>
-            <input type="radio" id="frau-anrede" name="anrede" value="Frau">
-            <label for="frau-anrede">Frau</label>
+        <div class="mb-3">
+            <div class ="form-check-label form-check-inline">Anrede:</div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" id="herr-andrede" name="anrede" value="Herr">
+                <label class="form-label" for="herr-andrede" >Herr</label>
+            </div>
+            <div class="form-check-inline">
+                <input class="form-check-input" type="radio" id="frau-anrede" name="anrede" value="Frau">
+                <label class="form-label" for="frau-anrede" >Frau</label>
+            </div>
         </div>
-        <div class="form-div-block">
-            <label for="sender-name">Name*</label>
+        <div class="mb-3">
+            <label class="form-label" for="sender-name">Name*</label>
             <br/>
-            <input class="email-text-field" type="text" id="sender-name" name="sender-name" placeholder=" Max Mustermann" required>
+            <input class="form-control" type="text" id="sender-name" name="sender-name" placeholder="Max Mustermann" required>
         </div>
-        <div class="form-div-block">
-            <label for="sender-email">Email*</label><br/>
-            <input class="email-text-field" type="email" id="sender-email" name="sender-email" placeholder=" ihre@email.com" required>
+        <div class="mb-3">
+            <label class="form-label" for="sender-email" >Email*</label><br/>
+            <input class="form-control" type="email" id="sender-email" name="sender-email" placeholder="ihre@email.com" required>
         </div>
-        <div class="form-div-block">
-            <label for="sender-nachricht">Nachricht*</label><br/>
-            <textarea class="email-text-field" id="sender-nachricht" name="sender-nachricht" placeholder=" Schreiben Ihre E-mail hier." required></textarea>
-            <p style="font-size: 8pt; text-align: left;">* is a required field</p>
+        <div class="mb-3">
+            <label class="form-label" for="sender-nachricht" >Nachricht*</label><br/>
+            <textarea class="form-control" id="sender-nachricht" name="sender-nachricht" placeholder="Schreiben Ihre E-mail hier." required></textarea>
+            <div class="form-text">* is a required field</div>
         </div>
-        <button type="submit" id="btn-sender-schicken" name="btn-sender-schicken" >Schicken</button>
+        <div>
+            <button class="btn btn-primary" type="submit" name="btn-sender-schicken" >Schicken</button>
+        </div>
     </form>
 </div>
 </body>
