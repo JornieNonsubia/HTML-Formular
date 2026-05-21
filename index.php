829cc7da93a78912f958de4a1bf0ef5022ea4b4a
Mykhailo <misha09107@gmail.com>
2026-05-21 16:37:29 +0200

    bootstrap fix v2

--- index.php
+++ index.php
@@ -44,6 +44,8 @@ if (is_array($_POST) && count($_POST))
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>E-mail Service</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <style>
         :root{
             --colors--shapeblau: #71c8da;
